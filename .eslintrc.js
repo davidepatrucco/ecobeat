9bf74b3accdd0eebe4315a30f7a27cdec0698b3d
davidepatrucco <davidepatrucco@gmail.com>
2025-08-28 22:43:03 +0200

    commit pre-app mobile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,57 @@
 module.exports = {
   root: true,
-  extends: [
-    'eslint:recommended',
-    '@typescript-eslint/recommended',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'prettier'],
+  parser: require.resolve('@typescript-eslint/parser'),
   plugins: ['@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   env: {
     es6: true,
     node: true,
+    browser: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    // TypeScript rules (manually configured instead of extending)
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/prefer-as-const': 'error',
+    '@typescript-eslint/no-array-constructor': 'error',
+    '@typescript-eslint/no-empty-function': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'warn',
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-namespace': 'warn',
+    '@typescript-eslint/no-this-alias': 'error',
+    '@typescript-eslint/no-unnecessary-type-constraint': 'error',
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+    '@typescript-eslint/triple-slash-reference': 'error',
+
+    // General JavaScript rules
+    'no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
     'prefer-const': 'error',
     'no-var': 'error',
+    'no-console': 'off',
+    'no-debugger': 'warn',
   },
   ignorePatterns: [
     'node_modules/',
@@ -29,5 +59,7 @@ module.exports = {
     'build/',
     '.expo/',
     '*.generated.*',
+    'cdk.out/',
+    'coverage/',
   ],
 };
